c50e59e8f29ede499a0b678a513c0b5be541f203
ThaiLe <thailevlfyb@gmail.com>
2024-02-15 11:15:23 +0700

    Change db host

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -4,11 +4,16 @@ const connection = (function () {
   let sequelize;
 
   const init = async () => {
-    sequelize = new Sequelize("chat_app_db", "postgres", "hikiganha9", {
-      host: "localhost",
-      dialect: "postgres",
-      logging: false,
-    });
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER_NAME,
+      process.env.DB_PASSWORD,
+      {
+        host: "postgres://root:TF5Rnn7mN19jRYTntn2O8maWEJaeQb55@dpg-cn6ol0qcn0vc73dllbb0-a/db_chatapp",
+        dialect: "postgres",
+        logging: false,
+      }
+    );
   };
 
   const testConnection = async () => {
