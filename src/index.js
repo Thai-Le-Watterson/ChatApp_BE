964eece6406042b4ae281539464c518eb728c46f
ThaiLe <thailevlfyb@gmail.com>
2024-02-19 10:21:40 +0700

    Little change

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const whitelist = [process.env.CLIENT_URL];
 const corsOptions = {
   credentials: true,
   origin: function (origin, callback) {
-    console.log(origin);
+    console.log(origin, process.env.CLIENT_URL);
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
