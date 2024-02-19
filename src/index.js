db0dde8c941cce07c85af03f33422a3846501a54
ThaiLe <thailevlfyb@gmail.com>
2024-02-19 10:10:00 +0700

    Little change

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ initModels();
 
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  // res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
