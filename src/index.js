857f33f321689c4ac4816832fe550e5e9a05f524
ThaiLe <thailevlfyb@gmail.com>
2024-02-19 10:45:54 +0700

    Little change

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie"
+    "*"
+    // "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie"
   );
   next();
 });
